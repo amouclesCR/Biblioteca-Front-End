d61c6aab536627467c6060d19a453057b976df30
amoucles <alexcuatroojos@gmail.com>
2019-04-30 15:20:46 -0600

    cambio del modelo de usuarios

--- AMBU-Biblioteca/src/app/interfaces/usuario.ts
+++ AMBU-Biblioteca/src/app/interfaces/usuario.ts
@@ -1,8 +1,5 @@
 export interface Usuario {
     id: number;
-    usu_nombre_usuario: string;
     usu_clave: string;
-    usu_nombre: string;
-    usu_apellidos: string;
     usu_identificacion: string;
 }
