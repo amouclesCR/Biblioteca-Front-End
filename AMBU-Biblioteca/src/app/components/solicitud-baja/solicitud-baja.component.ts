f5c4cb6920b8fadb6ac709036afa6b55bbd0e83f
amoucles <alexcuatroojos@gmail.com>
2019-05-01 13:50:27 -0600

    secciones de la solicitud

--- AMBU-Biblioteca/src/app/components/solicitud-baja/solicitud-baja.component.ts
+++ AMBU-Biblioteca/src/app/components/solicitud-baja/solicitud-baja.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataStorageService } from '../../services/index';
 @Component({
   selector: 'app-solicitud-baja',
   templateUrl: './solicitud-baja.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudBajaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dataStorageService: DataStorageService
+  ) { }
 
   ngOnInit() {
+    console.log(this.dataStorageService.getObjectValue("USUARIO"));
   }
-
 }
