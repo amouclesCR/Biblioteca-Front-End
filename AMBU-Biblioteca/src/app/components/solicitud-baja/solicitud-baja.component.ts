9dd1aa5b70e97dc54972cdaed3b1dc3044d76c17
amoucles <alexcuatroojos@gmail.com>
2019-05-09 19:42:52 -0600

    pef con usuario para traspasar actico

--- AMBU-Biblioteca/src/app/components/solicitud-baja/solicitud-baja.component.ts
+++ AMBU-Biblioteca/src/app/components/solicitud-baja/solicitud-baja.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { DataStorageService, ActivoService } from '../../services/index';
+import { DataStorageService, ActivoService, UsuarioService } from '../../services/index';
 import { Usuario, Activo } from 'src/app/interfaces/index';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import * as jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
 @Component({
@@ -17,19 +17,43 @@ export class SolicitudBajaComponent implements OnInit {
   private formGroupSolicitud: FormGroup;
   private listaActivos: Activo[];
   private listaActivosSolicitud: Activo[];
+  private listaUsuario: Usuario[];
+  private isTraspaso: boolean;
+  private nuevoUsuario: string;
 
 
   constructor(
     private dataStorageService: DataStorageService,
     private formBuilderSolicitud: FormBuilder,
-    private activoServicio: ActivoService
+    private activoServicio: ActivoService,
+    private usuarioService: UsuarioService
   ) { }
 
   // FUNCIONES
   IniciarFormulario() {
     this.formGroupSolicitud = this.formBuilderSolicitud.group({
-      numeroFormulario: ['', Validators.required]
+      numeroFormulario: ['', Validators.required],
+      tipoSolicitud: [this.isTraspaso]
     });
+  }
+
+  checkedChange() {
+    this.isTraspaso = !this.isTraspaso;
+    if (this.isTraspaso) {
+      this.agregarNuevoCampo();
+    } else {
+      this.eliminarNuevoCampo();
+    }
+  }
+
+  agregarNuevoCampo() {
+    this.formGroupSolicitud.addControl(
+      'usuario', new FormControl('', Validators.required)
+    );
+  }
+
+  eliminarNuevoCampo() {
+    this.formGroupSolicitud.removeControl('usuario');
   }
 
   getActivos() {
@@ -40,6 +64,15 @@ export class SolicitudBajaComponent implements OnInit {
     );
   }
 
+  getUsuarios() {
+    this.usuarioService.GetUsuarios().subscribe(
+      res => {
+        let index = res.body.findIndex(item => item.id == this.usuario.id);
+        this.listaUsuario = res.body;
+        this.listaUsuario.splice(index, 1);
+      });
+  }
+
   agregarActivoSolicitud(id: number) {
     let index = this.listaActivosSolicitud.findIndex(item => item.id == id);
     if (index > -1) {
@@ -48,6 +81,20 @@ export class SolicitudBajaComponent implements OnInit {
       let activo = this.listaActivos.find(item => item.id == id);
       this.listaActivosSolicitud.push(activo);
     }
+  }
+
+  submit() {
+    this.captureScreen();
+  }
+
+  usuarioSelecionado(id: number){
+    if (id) {
+     this.nuevoUsuario = this.listaUsuario.find(item => item.id == id).usu_identificacion;
+    }
+  }
+
+  get isFormularioValido() {
+    return this.formGroupSolicitud.invalid || this.listaActivosSolicitud.length == 0;
   }
 
   get fGSolicitud() {
@@ -75,6 +122,7 @@ ngOnInit() {
   this.usuario = this.dataStorageService.getObjectValue("USUARIO");
   this.IniciarFormulario();
   this.getActivos();
+  this.getUsuarios();
   this.listaActivosSolicitud = [];
 }
 }
